94cefd88d8550df41bb35e740e43422c2fda5510
lishaobos <www.long126@qq.com>
2022-12-23 17:39:45 +0800

    fix: del comment

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
-import 'uno.css' // css 原子化
+import 'uno.css'
 
 const app = createApp(App)
 app.use(createPinia())
